e7d6d6e053937efff7dd46d56b759ffa7d9e5778
Ballustrad <ethan.azoulay@e-artsup.net>
2023-01-26 20:39:12 +0100

    Remake

---
+++ Jon SnowMan/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private AudioSource Click;
+
+    public void PlayGame ()
+    {
+        Click.Play();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+    }
+
+    public void Credit ()
+    {
+        Click.Play();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        
+    }
+    public void Menu ()
+    {   
+        Click.Play();
+        SceneManager.LoadScene(0);
+        
+    }
+    public void QuitGame ()
+    {
+        Click.Play();
+        Debug.Log("Quit");  
+        Application.Quit();
+    }
+}
---
+++ Jon SnowMan/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameISPaused = false;
+    public GameObject pauseMenuUI;
+    public CameraShake cameraShake;
+
+    public GameManager gameManager;
+    
+    [SerializeField] private AudioSource Click;
+    [SerializeField] private AudioSource JB_Hard;
+    [SerializeField] private AudioSource BGM;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        { 
+            if (GameISPaused)
+            {
+                Resume();
+            }  else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume ()
+    {
+        Click.Play();
+        JB_Hard.UnPause();
+        BGM.UnPause();
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameISPaused = false;
+
+    }
+
+    void Pause ()
+    {   
+        Click.Play();
+        JB_Hard.Pause();
+        BGM.Pause();
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameISPaused = true;
+    }
+
+    public void LoadMenu()
+    {   
+        Click.Play();
+        SceneManager.LoadScene("Menu");
+    }
+}
+
